1549afa1a90c2581612ae793507ee1d1ae0665b1
tazim5032 <mfittazim@gmail.com>
2025-03-23 03:17:04 +0600

    blogs page added

---
+++ src/app/blogs/loading.tsx
@@ -0,0 +1,12 @@
+import Spinner from "@/components/ui/Spinner";
+
+const BlogsLoadingPage =  () => {
+  
+  return (
+    <div>
+        <Spinner/>
+    </div>
+  );
+};
+
+export default BlogsLoadingPage;
--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,8 +1,32 @@
-const BlogsPage = () => {
+import BlogCard from "@/components/ui/BlogCard";
+import { Blog } from "@/types";
+import { Metadata } from "next";
 
+export const metadata : Metadata = {
+  title: "NexaBlog | Blog",
+}
+
+const BlogsPage = async () => {
+  const res = await fetch("http://localhost:5000/blogs",{
+    cache: "no-store",
+  });
+  const blogs = await res.json();
   return (
-    <div>
-     <h1>This is blog page</h1>
+    <div className="w-[90%] mx-auto">
+      <h1 className="text-3xl text-center my-5 font-bold">
+        Explore All <span className="text-teal-600">Blogs</span>
+      </h1>
+      <p className="text-center text-gray-400 w-2/5 mx-auto">
+        <i>
+          Dive into the fascinating world of quantum computing, where unlocking
+          unprecedented computational power.
+        </i>
+      </p>
+      <div className="grid grid-cols-3 gap-6 my-5">
+        {
+          blogs.map((blog: Blog) => <BlogCard key={blog.id} blog={blog} /> )
+        }
+      </div>
     </div>
   );
 };
